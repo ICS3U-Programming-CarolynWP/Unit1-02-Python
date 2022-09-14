7d9c8a5906d410be861729ce27cc9a62240aa6e0
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-14 18:08:17 +0000

    Taught Kaitlin about pythons

--- hello_world_updated.py
+++ hello_world_updated.py
@@ -1,2 +1,2 @@
 print("Hello Kaitlin")
-print("Did you know that you're as cool as the Pythonidae, commonly known as pythons, which are a family of nonvenomous snakes found in Africa, Asia, and Australia, and that among its members are some of the largest snakes in the world, and ten genera and 42 species are currently recognized???")
+print("Did you know the Pythonidae, commonly known as pythons, are a family of nonvenomous snakes found in Africa, Asia, and Australia, that among its members are some of the largest snakes in the world, ten genera and 42 species are currently recognized???")
